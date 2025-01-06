789fd88c6eefce1c175a2d349b8fbb620d1ab39e
kevinirawand <kevinid07@gmail.com>
2025-01-06 11:06:05 +0700

    setup dev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: {
+    output: string;
+    images: { unoptimized: boolean };
+    assetPrefix: string;
+    basePath: string;
+    reactStrictMode: boolean
+} = {
+    reactStrictMode: false,
+    images: {
+        unoptimized: true,
+    },
+    output: process.env.NEXT_PUBLIC_OUTPUT,
+    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 export default nextConfig;
